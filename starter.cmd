2369f0bb73adfd4aa63e30becc2b5c626852b8a4
Adam Davies <adzdavies@gmail.com>
2017-04-07 16:49:53 +0930

    Create starter.cmd

---
+++ starter.cmd
@@ -0,0 +1,46 @@
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
+
+# Dev libs
+cinst javaruntime
+cinst DotNet4.5
+
+# Git
+cinst git.install -params '"/GitOnlyOnPath /NoAutoCrlf"'
+cinst poshgit
+cinst git-credential-winstore
+cinst beyondcompare
+cinst sourcetree
+
+cinst dropbox
+
+# Remote desktop
+cinst teamviewer
+
+# Video chat
+cinst skype
+
+# Editors / IDE
+cinst vim
+cinst atom
+cinst visualstudiocode
+cinst visualstudio2017community
+
+# Utils
+cinst 7zip
+cinst gow          # gnu-on-win
+cinst adobereader
+cinst fiddler4     # inspect http
+cinst sysinternals
+cinst cmder        # Cmd prompt
+
+# Browsers
+cinst GoogleChrome
+cinst Firefox
+
+
+# Maybe later?
+#cinst SqlServer2012Express
+#cinst IIS-WebServerRole -source windowsfeatures
+
+Install-WindowsUpdate
+Set-ExecutionPolicy -Force Unrestricted
